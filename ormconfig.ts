c9b3b2508ed0f8795f9ee763e45109732e20c85a
karima <karima@risk.az>
2022-03-11 21:48:50 +0400

    migrations mysql

--- ormconfig.ts
+++ ormconfig.ts
@@ -3,9 +3,9 @@ import { MysqlConnectionOptions } from "typeorm/driver/mysql/MysqlConnectionOpti
 //TODO: use env here
 const config: MysqlConnectionOptions = {
     name: 'default',
-    host: '127.0.0.1',
+    host: 'mysql-database',
     database: 'db',
-    port: 7006,
+    port: 3306,
     username: 'root',
     password: 'example',
     type: 'mysql',
