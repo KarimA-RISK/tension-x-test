f9d30e7a3908aad281be9450a751da3af15539f0
karima <karima@risk.az>
2022-03-11 21:46:24 +0400

    mg

--- ormconfig.ts
+++ ormconfig.ts
@@ -3,9 +3,9 @@ import { MysqlConnectionOptions } from "typeorm/driver/mysql/MysqlConnectionOpti
 //TODO: use env here
 const config: MysqlConnectionOptions = {
     name: 'default',
-    host: 'mysql-database',
+    host: '127.0.0.1',
     database: 'db',
-    port: 3306,
+    port: 7006,
     username: 'root',
     password: 'example',
     type: 'mysql',
---
+++ src/migrations/1647020760592-migrations.ts
@@ -0,0 +1,15 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class migrations1647020760592 implements MigrationInterface {
+    name = 'migrations1647020760592'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE \`user_entity\` (\`id\` int NOT NULL AUTO_INCREMENT, \`email\` varchar(255) NOT NULL, \`password\` varchar(255) NOT NULL, \`roles\` enum ('guest', 'user', 'supervisor', 'admin') NOT NULL, UNIQUE INDEX \`IDX_415c35b9b3b6fe45a3b065030f\` (\`email\`), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP INDEX \`IDX_415c35b9b3b6fe45a3b065030f\` ON \`user_entity\``);
+        await queryRunner.query(`DROP TABLE \`user_entity\``);
+    }
+
+}
